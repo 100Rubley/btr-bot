19d87901e4a2a75e323518c51fe07d44157f359c
Anton Kazakov <toxa_98@inbox.ru>
2023-06-04 15:41:24 +0300

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const token = process.env.TOKEN
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message');
+});
